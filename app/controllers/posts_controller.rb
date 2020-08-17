e40822bd52b3328cf9d2e6959d52706c51763d47
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-17 14:02:26 +0900

    投稿機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
   def index
   end
+  def new
+    @post = Post.new
+  end
+  def create
+    Post.create(title: params[:post][:title], content: params[:post][:content])
+    redirect_to new_post_path
+  end
 end
