68a20fcbd8a8ec79a67f2376f2a5afaa19766922
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-17 20:25:58 +0900

    バリデーション作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_044805) do
+ActiveRecord::Schema.define(version: 2020_08_17_085922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_08_17_044805) do
     t.string "title"
     t.text "content"
     t.text "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
