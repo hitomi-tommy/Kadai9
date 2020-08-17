1e5f51053698e32ac7198f52a912c42a423bb58b
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-17 14:49:40 +0900

    パーシャル作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
@@ -14,13 +15,10 @@ class PostsController < ApplicationController
     end
   end
   def show
-    @post = Post.find(params[:id])
   end
   def edit
-    @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "編集しました！"
     else
@@ -30,5 +28,8 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:title, :content, :image)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
