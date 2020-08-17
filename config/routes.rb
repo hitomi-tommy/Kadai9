3f0614118e70e52c82fe556fb801ea8797bb14fc
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-17 20:45:49 +0900

    Sessionsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :posts do
     collection do
       post :confirm
     end
   end
+  resources :users, only: [:new, :create, :show]
 end
