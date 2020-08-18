cbe0dad14fec92d26447a63805f5dce0306acbcf
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-18 20:21:23 +0900

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # get 'sessions/new'
   resources :posts do
     collection do
       post :confirm
     end
   end
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
